2c51b6c71d8de8f55ac7f4b7698c755ef201bc91
pewo <peter.wirdemo@gmail.com>
2018-02-26 18:07:19 +0100

    added example

---
+++ example/09.tf
@@ -0,0 +1,14 @@
+#
+# Put this file in ~/.toggl/2018/09.tf (or current year/week)
+#
+date=20180226
+start=12:00
+proj=100
+comment=som working
+end=12:45
+
+date=20180225
+start=08:00
+proj=101
+comment=som more working
+end=16:00
