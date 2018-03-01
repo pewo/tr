632468d58aa4615bfe91c6f652109559151a9bad
pewo <peter.wirdemo@gmail.com>
2018-03-02 00:41:20 +0100

    Added HotKey

--- Toggl.pm
+++ Toggl.pm
@@ -45,6 +45,47 @@ sub new {
         return($self);
 }
 
+package HotKey;
+
+#https://docstore.mik.ua/orelly/perl4/cook/ch15_09.htm
+
+our @ISA = qw(Exporter);
+our @EXPORT = qw(cbreak cooked readkey);
+
+use strict;
+use POSIX qw(:termios_h);
+my ($term, $oterm, $echo, $noecho, $fd_stdin);
+
+$fd_stdin = fileno(STDIN);
+$term     = POSIX::Termios->new( );
+$term->getattr($fd_stdin);
+$oterm    = $term->getlflag( );
+
+$echo     = ECHO | ECHOK | ICANON;
+$noecho   = $oterm & ~$echo;
+
+sub cbreak {
+    $term->setlflag($noecho);  # ok, so i don't want echo either
+    $term->setcc(VTIME, 1);
+    $term->setattr($fd_stdin, TCSANOW);
+}
+
+sub cooked {
+    $term->setlflag($oterm);
+    $term->setcc(VTIME, 0);
+    $term->setattr($fd_stdin, TCSANOW);
+}
+
+sub readkey {
+    my $key = '';
+    cbreak( );
+    sysread(STDIN, $key, 1);
+    cooked( );
+    return $key;
+}
+
+END { cooked( ) }
+
 package Toggl;
 
 use strict;
@@ -54,7 +95,7 @@ use Storable qw(lock_store lock_retrieve);
 use POSIX;
 
 $Toggl::VERSION = '0.01';
-@Toggl::ISA = qw(Object);
+@Toggl::ISA = qw(Object HotKey);
 
 sub new {
         my $proto = shift;
@@ -463,9 +504,10 @@ sub menu {
 			}
 		}	
 	}
-	print "c <rec> for continue, n for new rec, q for quit: ";
+	print "c <rec> for continue, n for new rec, q for quit:\n";
 	
-	my $answer = readline(STDIN);
+	my $answer = $self->readkey;
+	#my $answer = readline(STDIN);
 	chomp($answer);
 	if ( $answer =~ /^q/i ) {
 		print "Quit...\n";	
